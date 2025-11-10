2efd80da5da343ca06c2b0c8a5c743b83f8c4186
ayush78490 <tarunsingh78490@gmail.com>
2025-11-10 22:01:02 +0530

    added cors gopredix.xyz

--- server/api/validateMarket.js
+++ server/api/validateMarket.js
@@ -21,6 +21,7 @@ const corsOptions = {
     'https://sigma-predection.vercel.app',
     'https://gopredix.vercel.app/',
     'https://sigma-prediction.vercel.app',
+    'https://www.gopredix.xyz/',
     'http://localhost:3000',
     'http://localhost:3001',
     'http://127.0.0.1:3000',
