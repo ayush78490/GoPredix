2fb2e1a3d7aa005dce99434adb0c70b42077ba53
ayush78490 <tarunsingh78490@gmail.com>
2025-11-16 16:10:21 +0530

    added server ip

--- server/api/validateMarket.js
+++ server/api/validateMarket.js
@@ -25,7 +25,9 @@ const corsOptions = {
     'http://localhost:3000',
     'http://localhost:3001',
     'http://127.0.0.1:3000',
-    'http://127.0.0.1:3001'
+    'http://127.0.0.1:3001',
+    'http://64.29.17.131'
+
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: [
